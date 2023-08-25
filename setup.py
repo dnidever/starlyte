1a98c24bb63815fe1fbd48eee710214baa4280c7
David Nidever <dnidever@lsst.org>
2023-08-25 16:25:50 -0600

    bumped version from 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages, Extension
 from setuptools.command.build_py import build_py as _build_py
 
 setup(name='popstar',
-      version='1.0.0',
+      version='1.0.1',
       author = 'David Nidever',
       author_email='dnidever@montana.edu',
       url = 'https://github.com/dnidever/popstar',
